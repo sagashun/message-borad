0791136760fed6c76cb918da8ef50744996e231f
sagashun <sagashun2014@gmail.com>
2016-02-09 09:10:12 +0000

    コミットメッセージ

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+before_action :set_message, only: [:edit, :update]
   def index
     @message = Message.new
     @messages = Message.all
@@ -15,7 +16,23 @@ class MessagesController < ApplicationController
     end
   end
   
+def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
+def set_message
+    @message = Message.find(params[:id])
+  end
 
   def message_params
     params.require(:message).permit(:name, :body)
