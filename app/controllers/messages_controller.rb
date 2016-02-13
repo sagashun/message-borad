43cf95b4d0444d7384da22786f7079d9ff8b92ee
sagashun <sagashun2014@gmail.com>
2016-02-13 06:39:49 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,43 +1,17 @@
 class MessagesController < ApplicationController
-before_action :set_message, only: [:edite, :destroy]
-  def index
-    @message = Message.new
-    @messages = Message.all
-  end
-  
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-      redirect_to root_url, notice: 'メッセージを保存しました'
-    else
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
-    end
-  end
-  
-def edit
-  end
-  def destroy
-    @message.destroy
-    redirect_to root_path, notice: 'メッセージを削除しました'
-  end 
-  def update
-    if @message.update(message_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
-    else
-      # 保存に失敗した場合は編集画面へ戻す
-      render 'edit'
-    end
-  end
+def index
+@message = Message.new
+@messages = Message.all
+end
 
-  private
-def set_message
-    @message = Message.find(params[:id])
-  end
+def create
+@message = Message.new(message_params)
+@message.save
+redirect_to root_path,notice:'save complete!!'
+end
 
-  def message_params
-    params.require(:message).permit(:name, :body)
-  end
+private
+def message_params
+params.require(:message).permit(:name,:body)
+end
 end
