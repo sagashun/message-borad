cfbcb38506999c0666436cdab00b1cae14f92fbc
sagashun <sagashun2014@gmail.com>
2016-02-14 10:53:39 +0000

    coment01

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
 # 名前は必須入力かつ20文字以内
   validates :name , :length => {:maximum => 20 } , :presence => "true"
-  # 内容は必須入力かつ2文字以上30文字以下
+# 内容は必須入力かつ2文字以上30文字以下
   validates :body , :length => {:minimum => 2, :maximum => 30 } , :presence => "true"
+# 名前は必須入力かつ0以上の数字
+  validates :age  , :length => {:maximum => 3} ,:presence => "true" ,:numericality => "true"
 end
